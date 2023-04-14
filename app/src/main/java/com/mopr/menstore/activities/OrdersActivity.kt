2c5540c53f82a79990999bc4644ab8cba81eb4ac
mytran324 <92320923+mytran324@users.noreply.github.com>
2023-04-14 09:14:49 +0700

    OrderActivity

---
+++ app/src/main/java/com/mopr/menstore/activities/OrdersActivity.kt
@@ -0,0 +1,11 @@
+package com.mopr.menstore.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OrdersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_orders)
+    }
+}
